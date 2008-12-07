c209f5bd669933d6940f19eae8d6426f23ba461e
lobraun <lobraun@9b7ec49d-eebb-4683-924c-5cfa09519806>
2008-12-07 19:28:43 +0000

    created size_dumper data module, implemented init and finish functions
    git-svn-id: https://pcapsplit.svn.sourceforge.net/svnroot/pcapsplit@21 9b7ec49d-eebb-4683-924c-5cfa09519806

--- size_dumper.c
+++ size_dumper.c
@@ -1,12 +1,30 @@
 #include "size_dumper.h"
 
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_FILENAME 65535
+
+struct size_dumper_data {
+	char base_filename[MAX_FILENAME];
+	char dump_filename[MAX_FILENAME];
+	size_t number;
+};
+
 int size_dumper_init(struct dumping_module* m, void* data)
 {
+	struct size_dumper_data* sdata = (struct size_dumper_data*)malloc(
+		sizeof(struct size_dumper_data));
+	strncpy(sdata->base_filename, (char*)data, MAX_FILENAME);
+	sdata->number = 0;
+	m->module_data = (void*)sdata;
 	return 0;
 }
 
 int size_dumper_finish(struct dumping_module* m)
 {
+	free(m->module_data);
+	m->module_data = NULL;
 	return 0;
 }
 
