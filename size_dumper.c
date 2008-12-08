7dc3c688f2f0923a3fc94e30606ec91a9d3634c7
lobraun <lobraun@9b7ec49d-eebb-4683-924c-5cfa09519806>
2008-12-08 09:12:54 +0000

    implemented packet dumping in size_dumper
    git-svn-id: https://pcapsplit.svn.sourceforge.net/svnroot/pcapsplit@23 9b7ec49d-eebb-4683-924c-5cfa09519806

--- size_dumper.c
+++ size_dumper.c
@@ -24,7 +24,22 @@ struct size_dumper_data {
 	char base_filename[MAX_FILENAME];
 	char dump_filename[MAX_FILENAME];
 	size_t number;
+	size_t file_data_count;
+	size_t max_file_data_count;
+	pcap_t* out;
+	pcap_dumper_t* dumper;
 };
+
+void createNewFile(struct size_dumper_data* data)
+{
+	snprintf(data->dump_filename, MAX_FILENAME, "%s.%lu",
+		data->base_filename, data->number);
+	//TODO: use DLT from source file
+	data->out = pcap_open_dead(DLT_EN10MB, 65535);
+	data->dumper = pcap_dump_open(data->out, data->dump_filename);
+	data->number++;
+	data->file_data_count = 0;
+}
 
 int size_dumper_init(struct dumping_module* m, void* data)
 {
@@ -32,7 +47,13 @@ int size_dumper_init(struct dumping_module* m, void* data)
 		sizeof(struct size_dumper_data));
 	strncpy(sdata->base_filename, (char*)data, MAX_FILENAME);
 	sdata->number = 0;
+	sdata->file_data_count = 0;
+	// TODO: make configurable
+	sdata->max_file_data_count = 1000;
+	createNewFile(sdata);
+
 	m->module_data = (void*)sdata;
+
 	return 0;
 }
 
@@ -45,5 +66,13 @@ int size_dumper_finish(struct dumping_module* m)
 
 int size_dumper_run(struct dumping_module* m, struct packet* p)
 {
+	struct size_dumper_data* d = (struct size_dumper_data*)m->module_data;
+	pcap_dump((unsigned char*)d->dumper, &p->header, p->data);
+	d->file_data_count += p->header.len;
+	if (d->file_data_count >= d->max_file_data_count) {
+		pcap_dump_flush(d->dumper);
+		pcap_dump_close(d->dumper);
+		createNewFile(d);
+	}
 	return 0;
 }
