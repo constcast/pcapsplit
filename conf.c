770fccd28be69362809fbe88d6c607acca54fcb0
lobraun <lobraun@9b7ec49d-eebb-4683-924c-5cfa09519806>
2008-12-10 21:39:26 +0000

    copyright messages on conf.*
    git-svn-id: https://pcapsplit.svn.sourceforge.net/svnroot/pcapsplit@41 9b7ec49d-eebb-4683-924c-5cfa09519806

--- conf.c
+++ conf.c
@@ -1,5 +1,22 @@
+//  Copyright (C) 2008 Lothar Braun <lothar@lobraun.de>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "conf.h"
 #include "iniparser.h"
+
+#include <stdlib.h>
 
 struct config {
 	dictionary* d;
@@ -7,13 +24,16 @@ struct config {
 
 struct config* config_new(const char* file)
 {
-	return NULL;
+	struct config* ret = (struct config*)malloc(sizeof(struct config));
+	ret->d = iniparser_new(file);
+	return ret;
 }
 
 
 void config_free(struct config* config)
 {
-
+	iniparser_free(config->d);
+	free(config);
 }
 
 const char** config_get_module_names(struct config* config)
