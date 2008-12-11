db384e1a29ca71e9e18a49d539db5cc303514ddb
lobraun <lobraun@9b7ec49d-eebb-4683-924c-5cfa09519806>
2008-12-11 10:31:54 +0000

    added module for collecting and creating dumping_modules
    git-svn-id: https://pcapsplit.svn.sourceforge.net/svnroot/pcapsplit@48 9b7ec49d-eebb-4683-924c-5cfa09519806

---
+++ module_list.h
@@ -0,0 +1,23 @@
+//  Copyright (C) 2008 Lothar Braun <lothar@lobraun.de>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _MODULE_LIST_H_
+#define _MODULE_LIST_H_
+
+struct dumping_module;
+
+struct dumping_module* get_module(const char* name);
+
+#endif
