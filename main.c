e8709c0eb8c14c138735ccaa67f5b2b33347fe86
lobraun <lobraun@9b7ec49d-eebb-4683-924c-5cfa09519806>
2008-12-11 09:27:07 +0000

    compile fix
    git-svn-id: https://pcapsplit.svn.sourceforge.net/svnroot/pcapsplit@45 9b7ec49d-eebb-4683-924c-5cfa09519806

--- main.c
+++ main.c
@@ -14,7 +14,6 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "packet.h"
-#include "dumping_list.h"
 #include "dumping_module.h"
 #include "size_dumper.h"
 
