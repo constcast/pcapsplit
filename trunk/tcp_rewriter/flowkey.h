681f5481c1e621b331e26fb6cb82bd5e1c8ae136
lobraun <lobraun@9b7ec49d-eebb-4683-924c-5cfa09519806>
2009-01-24 11:14:52 +0000

    format fix
    git-svn-id: https://pcapsplit.svn.sourceforge.net/svnroot/pcapsplit@66 9b7ec49d-eebb-4683-924c-5cfa09519806

--- trunk/tcp_rewriter/flowkey.h
+++ trunk/tcp_rewriter/flowkey.h
@@ -12,75 +12,75 @@
 
 class TcpFlowKey
 {
-	public:
-		uint8_t data[FLOW_KEY_LEN];
-		const unsigned len;
+public:
+	uint8_t data[FLOW_KEY_LEN];
+	const unsigned len;
 
-		struct FlowKey
-		{
-			uint32_t srcIp;
-			uint32_t dstIp;
-			uint16_t srcPort;
-			uint16_t dstPort;
-		};
+	struct FlowKey
+	{
+		uint32_t srcIp;
+		uint32_t dstIp;
+		uint16_t srcPort;
+		uint16_t dstPort;
+	};
 
-		TcpFlowKey() : len(FLOW_KEY_LEN)
-		{
-			memset(data,0,sizeof(data));
+	TcpFlowKey() : len(FLOW_KEY_LEN)
+	{
+		memset(data,0,sizeof(data));
+	}
+
+	TcpFlowKey(const uint8_t* packet_data, bool reverseOrder = false, uint8_t ip_offset = 14) 
+		: len(FLOW_KEY_LEN), is_tcp(false)
+	{
+		memset(data,0, sizeof(data));
+		uint32_t ip1, ip2;
+		uint16_t port1, port2;
+		struct ip* iph = (struct ip*)(packet_data + ip_offset);
+		// TODO: Handle fragments!!!!
+		// TODO: handle ipv6
+		if (iph->ip_p != IPPROTO_TCP || iph->ip_v != 4) {
+			return;
 		}
-
-		TcpFlowKey(const uint8_t* packet_data, bool reverseOrder = false, uint8_t ip_offset = 14) 
-			: len(FLOW_KEY_LEN), is_tcp(false)
-		{
-			memset(data,0, sizeof(data));
-			uint32_t ip1, ip2;
-			uint16_t port1, port2;
-			struct ip* iph = (struct ip*)(packet_data + ip_offset);
-			// TODO: Handle fragments!!!!
-			// TODO: handle ipv6
-			if (iph->ip_p != IPPROTO_TCP || iph->ip_v != 4) {
-				return;
-			}
-			is_tcp = true;
-			struct tcphdr* tcph = (struct tcphdr*)(iph + iph->ip_hl);
-			if (!reverseOrder) {
-				getFlowKey()->srcIp = *(uint32_t*)&iph->ip_src;
-				getFlowKey()->dstIp = *(uint32_t*)&iph->ip_dst;
-				getFlowKey()->srcPort = tcph->th_sport;
-				getFlowKey()->dstPort = tcph->th_dport;
-			} else {
-				getFlowKey()->srcIp = *(uint32_t*)&iph->ip_dst;
-				getFlowKey()->dstIp = *(uint32_t*)&iph->ip_src;
-				getFlowKey()->srcPort = tcph->th_dport;
-				getFlowKey()->dstPort = tcph->th_sport;
-			}
-			seq = tcph->th_seq;
-			ack = tcph->th_ack;
-			packet_len = iph->ip_len; // TODO: calculate payload len instead ...
+		is_tcp = true;
+		struct tcphdr* tcph = (struct tcphdr*)(iph + iph->ip_hl);
+		if (!reverseOrder) {
+			getFlowKey()->srcIp = *(uint32_t*)&iph->ip_src;
+			getFlowKey()->dstIp = *(uint32_t*)&iph->ip_dst;
+			getFlowKey()->srcPort = tcph->th_sport;
+			getFlowKey()->dstPort = tcph->th_dport;
+		} else {
+			getFlowKey()->srcIp = *(uint32_t*)&iph->ip_dst;
+			getFlowKey()->dstIp = *(uint32_t*)&iph->ip_src;
+			getFlowKey()->srcPort = tcph->th_dport;
+			getFlowKey()->dstPort = tcph->th_sport;
 		}
+		seq = tcph->th_seq;
+		ack = tcph->th_ack;
+		packet_len = iph->ip_len; // TODO: calculate payload len instead ...
+	}
 
-		bool isTCP() { return is_tcp; }
+	bool isTCP() { return is_tcp; }
 
-		inline FlowKey* getFlowKey()
-		{
-			return (FlowKey*)data;
-		}
+	inline FlowKey* getFlowKey()
+	{
+		return (FlowKey*)data;
+	}
 
-		void reset()
-		{
-			memset(data,0,sizeof(data));
-		}
+	void reset()
+	{
+		memset(data,0,sizeof(data));
+	}
 
-		bool operator<(const TcpFlowKey& other) const 
-		{
-			return memcmp(data, other.data, len)<0?true:false;
-		}
-		
-		uint32_t seq;
-		uint32_t ack;
-		uint16_t packet_len;
-	private:
-		bool is_tcp;
+	bool operator<(const TcpFlowKey& other) const 
+	{
+		return memcmp(data, other.data, len)<0?true:false;
+	}
+	
+	uint32_t seq;
+	uint32_t ack;
+	uint16_t packet_len;
+private:
+	bool is_tcp;
 };
 
 #endif
