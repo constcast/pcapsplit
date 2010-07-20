da32f6aff9db438f51c7d255f3f28b2419a890a9
Lothar Braun <lothar@lobraun.de>
2010-07-20 15:02:03 +0200

    parse filter_dumper configuration

--- pcapsplit/modules/filter_dumper.c
+++ pcapsplit/modules/filter_dumper.c
@@ -16,9 +16,12 @@
 #include "filter_dumper.h"
 
 #include <tools/list.h>
+#include <tools/pcap-tools.h>
+#include <module_list.h>
 
 #include <stdlib.h>
 #include <string.h>
+#include <pcap.h>
 
 #define MAX_FILENAME 65535
 
@@ -33,12 +36,71 @@ struct dumping_module* filter_dumper_new()
 
 struct filter_dumper_data {
 	list_t* filter_list;
-	int i;
+	pcap_t* p;
 };
+
+struct filter_element {
+	struct bpf_program filter_program;
+	struct dumper_tool* dumper;
+};
+	
 
 int filter_dumper_init(struct dumping_module* m, struct config* c)
 {
 	struct filter_dumper_data* sdata = (struct filter_dumper_data*)malloc(sizeof(struct filter_dumper_data));
+	int class_no = 0;
+	char conf_name[MAX_FILENAME];
+	const char* class_name;
+	const char* filter_string;
+	char pcap_file[MAX_FILENAME];
+	const char* prefix = config_get_option(c, FILTER_DUMPER_NAME, "file_prefix");
+	
+	sdata->filter_list = list_create();
+	if (!sdata->filter_list) {
+		fprintf(stderr, "Could not create filter_list\n");
+		// TODO: cleanup
+		return -1;
+	}
+	sdata->p = pcap_open_dead(m->linktype, 65535);
+
+	// TODO: Introdcude a config parameter fro configuring the number of classes because the code below sucks
+	// build filters from module confiugration
+	// open pcap files for every defined class
+	do {
+		class_no++;
+		snprintf(conf_name, MAX_FILENAME, "class%d", class_no);
+		class_name = config_get_option(c, FILTER_DUMPER_NAME, conf_name);
+		if (!class_name)
+			continue;
+
+		snprintf(conf_name, MAX_FILENAME, "filter%d", class_no);
+		filter_string = config_get_option(c, FILTER_DUMPER_NAME, conf_name);
+		if (!filter_string) {
+			fprintf(stderr, "Could not find filter expression for class %s. Cannot recover from that!\n", class_name);
+			// TODO: cleanup allocated memory ...!
+			// TODO2: We could recover .... !
+			return -1;
+		}
+		struct list_element_t* le = (struct list_element_t*)malloc(sizeof(struct list_element_t));
+		struct filter_element* f = (struct filter_element*)malloc(sizeof(struct filter_element));
+		if (-1 == pcap_compile(sdata->p, &f->filter_program, filter_string,  0, 0)) { // TODO: check whether optimize in pcap_compile could be usefull
+			fprintf(stderr, "Could not compile pcap filter %s: %s\n", filter_string, pcap_geterr(sdata->p));
+			// TODO: cleanup this one, too 
+			return -1;
+		}
+
+		// open pcap file
+		snprintf(pcap_file, MAX_FILENAME, "%s%s", prefix, class_name);
+		f->dumper = dumper_tool_open_file(pcap_file, m->linktype);
+		if (!f->dumper) {
+			fprintf(stderr, "Cannot open pcap file\n");
+			//TODO: cleanup
+			return -1;
+		}
+	
+		le->data = f;
+		list_push_back(sdata->filter_list, le);
+	} while (class_name);
 
 	m->module_data = (void*)sdata;
 
