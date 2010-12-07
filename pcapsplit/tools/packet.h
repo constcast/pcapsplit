598ec97674d9f98dc23fb5f92c771fb8dbd1eaba
Lothar Braun <lothar.braun@googlemail.com>
2010-12-07 17:02:47 +0100

    compile fix for FreeBSD

--- pcapsplit/tools/packet.h
+++ pcapsplit/tools/packet.h
@@ -20,11 +20,14 @@
 #include <tools/list.h>
 
 #include <net/ethernet.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/ip6.h>
 #ifdef linux
 #define __FAVOR_BSD
 #endif
+
 #include <netinet/udp.h>
 #include <netinet/tcp.h>
 
