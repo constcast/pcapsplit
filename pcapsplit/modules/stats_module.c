f9805f83590c42781c712764de3444d05f303c66
Lothar Braun <lothar@lobraun.de>
2011-01-24 15:59:54 +0100

    configure statistics

--- pcapsplit/modules/stats_module.c
+++ pcapsplit/modules/stats_module.c
@@ -18,8 +18,10 @@
 #include <module_list.h>
 #include <tools/connection.h>
 #include <tools/msg.h>
+#include <tools/conf.h>
 
 #include <stdlib.h>
+#include <math.h>
 
 struct dumping_module* stats_module_new()
 {
@@ -31,29 +33,88 @@ struct dumping_module* stats_module_new()
 	return ret;
 }
 
-static uint64_t conn_num = 0;
+struct stats_module_data {
+	uint64_t total_data;
+	uint32_t min;
+	uint32_t max;
+	uint32_t binwidth;
+	uint32_t* bins;
+	uint32_t bin_count;
+} sm_data;
+
 static uint8_t has_init = 0;
 
 static void stats_connection_finished(struct connection* c)
 {
-	msg(MSG_DEBUG, "Conn: %llu %u %u %llu", conn_num, c->first_seen, c->last_seen, c->traffic_seen);
-	conn_num++;
+	//msg(MSG_DEBUG, "Conn: %llu %u %u %llu", conn_num, c->first_seen, c->last_seen, c->traffic_seen);
+	//conn_num++;
 }
 
-int stats_module_init(struct dumping_module* m, struct config* data)
+int stats_module_init(struct dumping_module* m, struct config* c)
 {
+	// we can only run one instance of the stats_module. If this has already been allocated,
+	// then we have no other choice than exiting ...
 	if (has_init) {
 		msg(MSG_FATAL, "stats module has already been initiatlized. It may not be instanciated twice ...");
 		return -1;
 	}
+
 	has_init = 1;
-	conn_num = 0;
+
+	uint32_t min, max, binwidth, i;
+	const char* tmp;
+
+	tmp = config_get_option(c, STATS_MODULE_NAME, "min");
+	if (tmp) {
+		min = atoi(tmp);
+	} else {
+		msg(MSG_FATAL, "%s: Could not find configuration for \"min\"", STATS_MODULE_NAME);
+		return -1;
+	}
+
+	tmp = config_get_option(c, STATS_MODULE_NAME, "max");
+	if (tmp) {
+		max = atoi(tmp);
+	} else {
+		msg(MSG_FATAL, "%s: Could not find configuration for \"max\"", STATS_MODULE_NAME);
+		return -1;
+	}
+
+	if (max < min) {
+		msg(MSG_FATAL, "%s: Max < Min!!!!!", STATS_MODULE_NAME);
+		return -1;
+	}
+
+	tmp = config_get_option(c, STATS_MODULE_NAME, "binwidth");
+	if (tmp) {
+		binwidth = atoi(tmp);
+	} else {
+		msg(MSG_FATAL, "%s: Could not find configuration for \"binwidth\"", STATS_MODULE_NAME);
+		return -1;
+	}
+
+	if (binwidth == 0) {
+		msg(MSG_FATAL, "%s: Binwidth == 0. This is stupid!", STATS_MODULE_NAME);
+		return -1;
+	}
+
+	sm_data.min = min;
+	sm_data.max = max;
+	sm_data.binwidth = binwidth;
+	sm_data.total_data = 0;
+	sm_data.bin_count = (max - min) / binwidth;
+	sm_data.bins = malloc(sizeof(uint32_t) * sm_data.bin_count);
+	for (i = 0; i != sm_data.bin_count; ++i) {
+		sm_data.bins[i] = 0;
+	}
+
 	conn_finished_cb = stats_connection_finished;
 	return 0;
 }
 
 int stats_module_finish(struct dumping_module* m)
 {
+	free(sm_data.bins);
 	return 0;
 }
 
